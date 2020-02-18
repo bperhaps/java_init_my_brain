ba1ddc39515efec850e2258ef204db664d53b4d8
Minsung Son <sns2831@gmail.com>
2020-02-19 01:29:34 +0900

    FIRST DAY page 46.

---
+++ src/lecture1/Hello.java
@@ -0,0 +1,17 @@
+package lecture1;
+
+/*
+작성자 : 손민성
+작성일 : 2020.02.19
+
+나는 정말 자바라는 언어를 이해하고 있는걸까?
+이것이 자바다 정주행.
+ */
+
+public class Hello {
+    //프로그램 실행 진입점
+    public static void main(String[] args) {
+        //콘솔에 출력하는 실행문
+        System.out.println("Hello, welcome to the java world!");
+    } //end of main
+} //end of class
