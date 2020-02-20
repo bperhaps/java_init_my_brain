698e5a50e2ebe490fddc9fc168b9cd15c48972a5
Minsung Son <sns2831@gmail.com>
2020-02-20 23:12:32 +0900

    SECOND DAY page 133.

---
+++ src/lecture3/StringEqualsExample.java
@@ -0,0 +1,17 @@
+package lecture3;
+
+public class StringEqualsExample {
+    public static void main(String args[]) {
+        String strVar1 = "손민성";
+        String strVar2 = "손민성";
+        String strVar3 = new String("손민성");
+
+        System.out.println( strVar1 == strVar2 );
+        System.out.println( strVar1 == strVar3 );
+        System.out.println();
+        System.out.println( strVar1.equals(strVar2) );
+        System.out.println( strVar1.equals(strVar2) );
+
+
+    }
+}
