ac746a64d44db10346615343355a5186b096ebe9
blesk011 <blesk0111@gmail.com>
2020-02-24 19:14:49 +0900

    update: lecture2

---
+++ src/blesk011/lecture2/VariableExample.java
@@ -0,0 +1,15 @@
+package blesk011.lecture2;
+
+public class VariableExample {
+	public static void main(String[] args) {
+		// 10을 변수 value의 초기값으로 저장
+		int value = 10;
+		
+		//변수 value의 값을 읽고 10을 더한느 산술 연산을 수행
+		//연산의 결과값을 변수 result의 초기값으로 저장
+		int result = value + 10;
+		
+		//변수  result의 값을 읽고 콘솔에 출력
+		System.out.println(result);
+	}
+}
